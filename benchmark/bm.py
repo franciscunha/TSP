bb20eb67b618cea8d6f882bfbb8ed5a3ff52c5e5
Francisco Cunha <francisccdn@gmail.com>
2019-11-19 22:17:51 -0300

    Benchmark automatization.

---
+++ benchmark/bm.py
@@ -0,0 +1,42 @@
+fin = open("bm.txt","r")
+fout = open("results.txt", "w+")
+
+i = 0
+flag = 0 # 0 is cost 1 is time
+for line in fin.readlines():
+    no_slash_n = line.split("\n")
+    my_list = no_slash_n[0].split()
+    
+    if len(my_list) == 1:
+        
+        if i > 0:
+            avg_cost = avg_cost/10
+            avg_time = avg_time/10
+
+            fout.write("\nAvg cost: ")
+            fout.write(str(avg_cost))
+            fout.write("\nAvg time: ")
+            fout.write(str(avg_time))
+            fout.write("\n")
+
+        fout.write(my_list[0])
+
+        avg_cost = 0
+        avg_time = 0
+
+    else:
+        num = my_list[1]
+        
+        if flag == 0:
+            avg_cost += int(num)
+            flag = 1
+        elif flag == 1:
+            avg_time += float(num)
+            flag = 0
+        
+    i += 1
+
+
+fin.close()
+fout.close()
+
--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo "Running TSP"
+echo "--TSP Benchmark--"
 
 make
 
-for instance in ./instances/*; do
-	echo $instance
+for instance in instances/*; do
+	echo $instance >> ./benchmark/bm.txt
 	for i in {1..10}; do
-		./tsp ${instance} | grep 'COST\|TIME' | awk "{print $1}"
+		./tsp ${instance} | grep 'COST\|TIME' | awk "{print $1}" >> ./benchmark/bm.txt
 	done
 
 done
+
+echo "-" >> ./benchmark/bm.txt
+
+cd benchmark
+python3 bm.py
+cd ..
+
+echo "Benchmark completed."
